04e85620dfdeb7663502caf0faa40a4a10dffeb0
Adarsh Dubey <adarshyt093@gmail.com>
2025-05-27 23:11:59 +0530

    fix: Update dependencies and config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['img.clerk.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
